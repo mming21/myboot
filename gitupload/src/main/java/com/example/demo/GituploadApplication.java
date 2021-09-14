93b94507e560911cd0541e93438ff97a61769c57
mming21 <mming1030@gmail.com>
2021-09-14 14:29:59 +0900

    1 commit

---
+++ gitupload/src/main/java/com/example/demo/GituploadApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GituploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GituploadApplication.class, args);
+		System.out.println("Mission Complete");
+	}
+
+}
